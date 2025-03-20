76cc45e1aa84b7e24e0670747020cd7b84f24329
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-03-20 18:21:18 +0530

    changed localhost to domain

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ connectDB()
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://localhost:4000/api'
+export const baseUrl = 'https://api.insourcextech.com/api'
