236345522d81566af02f8a365e94286d8f639c3c
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-03-20 13:25:22 +0530

    fixed bug

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    dotenv.config();
+    const conn = await mongoose.connect(process.env.MONGO_URI as string || "mongodb+srv://insourcextech:n8SpYofyDEtQT4BQ@cluster0.fblcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" );
     
     console.log(`MongoDB Connected success: ${conn.connection.host}`);
   } catch (error) {
