7381787149dc2e38b0d0c1011dc88a88ef488b42
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-03-22 14:43:54 +0530

    updated

--- backend/src/utils/emailServices.ts
+++ backend/src/utils/emailServices.ts
@@ -14,16 +14,19 @@ interface EmailOptions {
 }
 
 export const sendEmail = async (options: EmailOptions): Promise<void> => {
+  console.log("inside sendmial");
   const mail = process.env.EMAIL_USER;
   const pass = process.env.EMAIL_PASS
   // Create transporter
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: mail,
-      pass: pass,
+      user: "khizer.hyd@gmail.com",
+      pass: "mitb xtpb frai bwew",
     },
   });
+
+  console.log("after sendmail");
 
   // Mail options
   const mailOptions = {
