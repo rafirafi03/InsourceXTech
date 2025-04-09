0276cc0f78db9aea963a4dec3856a97066516098
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-04-09 21:37:33 +0530

    corrected company name

--- frontend/src/components/User/HeroSection/HeroSection.tsx
+++ frontend/src/components/User/HeroSection/HeroSection.tsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
           </span>
         </h1>
         <p className="text-lg md:text-xl text-black/50 mb-8 font-sans font-medium relative">
-          At Insoure X Tech, we deliver cutting-edge solutions that drive
+          At Insource X Tech, we deliver cutting-edge solutions that drive
           growth, efficiency, and digital transformation for businesses of all
           sizes.
         </p>
